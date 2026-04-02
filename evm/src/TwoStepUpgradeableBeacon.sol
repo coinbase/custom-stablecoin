2934f135f8e60a6bbb40e6e3b0bf59ce19bf3e5d
Braden Thompson <braden.thompson@coinbase.com>
2026-04-02 16:10:15 -0600

    fix: address spearbit audit findings

--- evm/src/TwoStepUpgradeableBeacon.sol
+++ evm/src/TwoStepUpgradeableBeacon.sol
@@ -20,7 +20,7 @@ contract TwoStepUpgradeableBeacon is UpgradeableBeacon, Ownable2Step {
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
     /// @notice Thrown when the owner attempts to renounce ownership.
-    error RenouneOwnershipDisabled();
+    error RenounceOwnershipDisabled();
 
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
     /*                        CONSTRUCTOR                         */
@@ -43,7 +43,7 @@ contract TwoStepUpgradeableBeacon is UpgradeableBeacon, Ownable2Step {
 
     /// @inheritdoc Ownable
     function renounceOwnership() public override(Ownable) onlyOwner {
-        revert RenouneOwnershipDisabled();
+        revert RenounceOwnershipDisabled();
     }
 
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
